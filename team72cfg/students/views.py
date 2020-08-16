bcda6e09dda77847f5e5bfc7b71f06c3985c24b1
nehalakhani1805 <neha.lakhani1805@gmail.com>
2020-08-16 13:45:15 +0530

    mystudents done

--- team72cfg/students/views.py
+++ team72cfg/students/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import Student
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required
 def mystudents(request, id):
-    print(id)
-    t=User.objects.filter(id=id).first()
-    print(t)
-    st=Student.objects.filter(teacher=t)
-    print(st)
-    return render(request, 'students/mystudents.html',{'st':st})
+    if request.user.id==id:
+        print(id)
+        t=User.objects.filter(id=id).first()
+        print(t)
+        st=Student.objects.filter(teacher=t)
+        print(st)
+        return render(request, 'students/mystudents.html',{'st':st})
+    else:
+        return render(request, 'students/error.html')
